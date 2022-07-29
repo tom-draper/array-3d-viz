35cfd09c7fd2c6b8b10e992fe69b1eef0eeb8158
Tom Draper <tomjdraper1@gmail.com>
2022-07-29 20:01:03 +0100

    Remove numpy generation

--- gen.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-x = np.array([[10, 20, 30], [40, 50, 60], [70, 80, 90], [110, 120, 130]])
-x = np.random.rand(3, 2)
-
-print(x)
-
-with open('data/data.npy', 'wb') as f:
-    np.save(f, x);
