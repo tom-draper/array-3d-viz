3a53ca58466f5aa4ce4d7c2240dc516f41e44744
Tom Draper <41476809+tom-draper@users.noreply.github.com>
2023-10-30 21:02:48 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ const fs = require("fs");
 const { PythonShell } = require("python-shell");
 
 function getFilePath() {
-  let path = process.argv[2];
+  const path = process.argv[2];
 
   // Default to json data format
-  if (path != undefined) {
-    if (!path.includes(".")) {
-      path = path + ".json";
-    }
-    if (!path.includes("data/")) {
-      path = "./data/" + path;
-    }
+  if (path === undefined) {
+    return undefined
+  }
+  if (!path.includes(".")) {
+    path = path + ".json";
+  }
+  if (!path.includes("data/")) {
+    path = "./data/" + path;
   }
   return path;
 }
@@ -27,35 +28,39 @@ async function fileExists(file) {
 }
 
 function convertToJSON(path) {
-  if (path == undefined) {
+  if (path === undefined) {
     return;
   }
-  let extension = path.split(".").slice(-1)[0];
-  if (extension == "json") {
-    fs.readFile(path, "utf8", (err, data) => {
-      if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-      } else {
-        fs.writeFile("data/temp/temp.json", data, "utf8", (err) => {
-          if (err) {
-            console.log(`Error writing file: ${err}`);
-          }
-        });
-      }
-    });
-  } else if (extension == "npy" || extension == "npz") {
-    // Run script to load target .npy file and save it in json format
-    PythonShell.run(
-      "scripts/load.py",
-      { args: [path] },
-      function (err, results) {
+  
+  const extension = path.split(".").slice(-1)[0];
+  switch (extension) {
+    case "json":
+      fs.readFile(path, "utf8", (err, data) => {
         if (err) {
-          console.log(err, results);
+          console.log(`Error reading file from disk: ${err}`);
+        } else {
+          fs.writeFile("data/temp/temp.json", data, "utf8", (err) => {
+            if (err) {
+              console.log(`Error writing file: ${err}`);
+            }
+          });
         }
-      }
-    );
-  } else {
-    throw "File type not supported.";
+      });
+      break;
+    case "npy", "npz":
+      // Run script to load target .npy file and save it in json format
+      PythonShell.run(
+        "scripts/load.py",
+        { args: [path] },
+        function (err, results) {
+          if (err) {
+            console.log(err, results);
+          }
+        }
+      );
+      break;
+    default:
+      throw "File type not supported.";
   }
 }
 
@@ -77,7 +82,7 @@ function run(gui) {
   if (!gui) {
     // To return array data read from file
     app.get("/data", function (req, res) {
-      let readable = fs.createReadStream("data/temp/temp.json");
+      const readable = fs.createReadStream("data/temp/temp.json");
       readable.pipe(res);
     });
   }
@@ -86,12 +91,12 @@ function run(gui) {
   console.log("Server started at: http://localhost:" + port);
 }
 
-let gui = process.argv.length != 3;
+const gui = process.argv.length != 3;
 
 if (gui) {
   run(gui);
 } else {
-  let path = getFilePath();
+  const path = getFilePath();
 
   if (fileExists(path)) {
     convertToJSON(path); // Save target data into data/temp.json
